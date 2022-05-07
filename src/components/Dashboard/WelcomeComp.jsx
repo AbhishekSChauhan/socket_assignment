f5d82373a708a130e7b2ffeb16472f234341f2d5
AbhishekSChauhan <abhishek.uietpuchd@gmail.com>
2022-05-07 18:02:49 +0530

    Welcome component

--- src/components/Dashboard/WelcomeComp.jsx
+++ src/components/Dashboard/WelcomeComp.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import './Welcome.css'
 
 const WelcomeComp = () => {
   return (
-    <div>WelcomeComp</div>
+    <div className='welcome-container'>
+      <div className='welcome-greet'>
+        Welcome back Anna
+      </div>
+      <div className='welcome-msg'>
+        You've learned 80% of your goal this week!
+      </div>
+      <div className='welcome-msg2'>
+        Keep it up and improve your results!
+      </div>
+    </div>
   )
 }
 
